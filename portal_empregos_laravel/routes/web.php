15f1a1f2f451836bd94b05b906ec34082d47877a
Edu1Sinico <EduardoSenai888@gmail.com>
2026-09-15 18:35:04 -0300

    Testando os acessos de usuário sem autenticação para a página home (middlewares) - 15/09/2026

--- portal_empregos_laravel/routes/web.php
+++ portal_empregos_laravel/routes/web.php
@@ -4,26 +4,27 @@ use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 // Rota de testes para a página 'home'
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
-})->name('home');
+})->middleware('auth')->name('home');
+// O middleware auth impede que o usuário acesse a página sem estar logado.
 
 // Rota para a tela de login (Apenas exibe o formulário, por isso o método GET)
 Route::get('/login', [UsuarioController::class, 'showLoginForm'])
-    ->name('usuarios.login'); // Indica a página que será acessada
+    ->name('login'); // Indica a rota (função) que será executado
 
 // Rota para a função de login
 Route::post('/login', [UsuarioController::class, 'login'])
-    ->name('usuarios.login.entrar'); // Indica a rota (função) que será executado
+    ->name('usuarios.login.entrar');
 
 // Rota para a tela de registro (Apenas exibe o formulário, por isso o método GET)
 Route::get('/registro', [UsuarioController::class, 'showRegistroForm'])
-    ->name('usuarios.registro'); // Indica a página que será acessada
+    ->name('usuarios.registro');
 
 // Rota para a função de cadastro
 Route::post('/registro', [UsuarioController::class, 'registro'])
-    ->name('usuarios.registro.salvar'); // Indica a rota (função) que será executado
+    ->name('usuarios.registro.salvar');
 
 // Rota para a função de logout
 Route::post('/logout', [UsuarioController::class, 'logout'])
-    ->name('usuarios.logout'); // Indica a rota (função) que será executado
+    ->name('usuarios.logout');
