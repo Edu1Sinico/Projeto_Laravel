6cb8b3058f37f8e892b6c5f4a6a938c220e3f40a
Edu1Sinico <EduardoSenai888@gmail.com>
2026-09-14 22:51:47 -0300

    Restruturando o 'UsuarioController' e criando as telas iniciais para testes - 14/09/2026

--- portal_empregos_laravel/app/Http/Controllers/UsuarioController.php
+++ portal_empregos_laravel/app/Http/Controllers/UsuarioController.php
@@ -4,80 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class UsuarioController extends Controller
 {
-    // Função para exibir a view de cadastro de usuário
-    public function cadastroView()
+    // Exibe o formulário de login
+    public function showLoginForm()
     {
-        return view('usuario.cadastro');
+        return view('usuarios.login');
     }
 
-    // Função de cadastro de usuário
-    public function cadastro(Request $request)
+    // Processa o login do usuário
+    public function login(Request $request) {}
+
+    // Exibe o formulário de registro
+    public function showRegistroForm()
     {
-        // Validação dos dados recebidos do formulário
-        $request->validate([
-            'nome' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:usuarios',
-            'senha' => 'required|string|min:6|confirmed',
-        ]);
-
-        // Criação do usuário
-        $usuario = Usuario::create([
-            'nome' => $request->nome,
-            'email' => $request->email,
-            'senha' => Hash::make($request->senha),
-        ]);
-
-        // Redirecionamento após o cadastro
-        return redirect()
-            ->route('usuario.login')
-            ->with('success', 'Usuário cadastrado com sucesso.');
+        return view('usuarios.registro');
     }
 
-    // Função para exibir a view de login de usuário
-    public function loginView()
-    {
-        return view('usuario.login');
-    }
+    // Processo o registro do usuário
+    public function registro(Request $request) {}
 
-    // Função de login de usuário
-    public function login(Request $request)
-    {
-        // Validação dos dados recebidos do formulário
-        $request->validate([
-            'email' => 'required|string|email',
-            'senha' => 'required|string',
-        ]);
-
-        // Verificação do usuário no banco de dados
-        $usuario = Usuario::where('email', $request->email)->first();
-
-        // Verificação da senha
-        if ($usuario && Hash::check($request->senha, $usuario->senha)) {
-            // Autenticação bem-sucedida, redirecionar para a página desejada
-            return redirect()
-                ->route('vagas.index')  // Redirecionar para a página de vagas após o login
-                ->with('success', 'Login realizado com sucesso.');
-        } else {
-            // Autenticação falhou, redirecionar de volta com mensagem de erro
-            return redirect()
-                ->back()
-                ->withErrors(['email' => 'Credenciais inválidas.']);
-        }
-    }
-
-    // Função de logout de usuário
-    public function logout(Request $request){
-        // Limpar a sessão do usuário
-        $request->session()->flush();
-
-        // Redirecionar para a página de login
-        return redirect()
-            ->route('usuario.login')
-            ->with('success', 'Logout realizado com sucesso.');
+    // Processa o logout do usuário
+    public function logout(Request $request) {
+        
     }
 }
--- portal_empregos_laravel/routes/web.php
+++ portal_empregos_laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rota para a tela de login (Apenas exibe o formulário, por isso o método GET)
+Route::get('/login', [UsuarioController::class, 'showLoginForm'])
+    ->name('usuarios.login'); // Indica a página que será acessada
+
+// Rota para a tela de registro (Apenas exibe o formulário, por isso o método GET)
+Route::get('/registro', [UsuarioController::class, 'showRegistroForm'])
+    ->name('usuarios.registro'); // Indica a página que será acessada
